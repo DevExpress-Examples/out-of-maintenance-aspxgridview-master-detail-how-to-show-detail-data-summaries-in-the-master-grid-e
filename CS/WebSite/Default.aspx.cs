92f6d8262d224be7d94260c83f159df54c0eb14c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void dsProducts_Init(object sender, EventArgs e) {
+        AccessDataSource dataSource = sender as AccessDataSource;
+        GridViewDetailRowTemplateContainer container = dataSource.NamingContainer as GridViewDetailRowTemplateContainer;
+
+        dataSource.SelectParameters["CategoryID"].DefaultValue = container.KeyValue.ToString();
+    }
+    protected void gvCategories_CustomUnboundColumnData(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs e) {
+        object categoryID = e.GetListSourceFieldValue("CategoryID");
+        if (e.Column.FieldName == "UnitsInStock") {
+            e.Value = DataHelper.ExecuteCommand(String.Format("SELECT SUM([UnitsInStock]) FROM  [Products] WHERE ([CategoryID] = {0})", categoryID));
+        }
+        if (e.Column.FieldName == "AverageUnitPrice") {
+            e.Value = DataHelper.ExecuteCommand(String.Format("SELECT AVG([UnitPrice]) FROM  [Products] WHERE ([CategoryID] = {0})", categoryID));
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - Master-Detail - How to show detail data summaries in the master grid
+
+
+<p>This example demonstrates how to display detail data summary values in the master grid's columns.</p><p><a href="http://documentation.devexpress.com/#AspNet/CustomDocument3732"><u>Unbound columns</u></a> are used to display detail data summaries.  The <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_CustomUnboundColumnDatatopic"><u>ASPxGridView.CustomUnboundColumnData</u></a> event handler is used to provide data for the unbound columns by calculating the detail data summaries using SQL queries.</p>
+
+<br/>
+
+
